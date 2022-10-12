beeebc540d12b01c2ef13051b657bb5557ed1a32
Renan Theodoro <renan.theodoro@simlabsdev.onmicrosoft.com>
2022-10-12 14:58:31 -0300

    feat: add button submit widget

---
+++ lib/presentation/components/button_submit.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ButtonSubmit extends StatelessWidget {
+  const ButtonSubmit({Key? key, this.callback}) : super(key: key);
+
+  final Function? callback;
+
+  @override
+  Widget build(BuildContext context) => ClipOval(
+        child: Material(
+          color: Colors.purple,
+          child: InkWell(
+            splashColor: Colors.purpleAccent,
+            onTap: () => callback!(),
+            child:
+                const SizedBox(width: 40, height: 40, child: Icon(Icons.send, color: Colors.white,)),
+          ),
+        ),
+      );
+}
