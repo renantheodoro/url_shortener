88d9527696dfca9960ab4c391241861c035d9164
Renan Theodoro <renan.theodoro@simlabsdev.onmicrosoft.com>
2022-10-12 14:54:59 -0300

    feat: add url remote datasource

---
+++ lib/data/datasource/remote/url_remote_data_source.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:url_shortener/core/consts/uri.const.dart';
+import 'package:url_shortener/core/error/exception.dart';
+import 'package:url_shortener/data/models/url_model.dart';
+
+abstract class UrlRemoteDataSourceInterface {
+  Future<UrlModel> shorten(String originalUrl);
+}
+
+class UrlRemoteDataSource
+    implements UrlRemoteDataSourceInterface {
+
+    UrlRemoteDataSource(this.client);
+
+  final http.Client client;
+
+  @override
+  Future<UrlModel> shorten(String originalUrl) async {
+    final Uri url = Uri.parse(uriPath);
+    final Map<String, String> headers = {'Content-Type': 'application/json'};
+    final String body = jsonEncode({"url": originalUrl});
+
+    final http.Response response =
+        await client.post(url, headers: headers, body: body);
+
+    if (response.statusCode != 200) {
+      throw ServerException();
+    }
+
+    return UrlModel.fromJson(response.body);
+  }
+}
