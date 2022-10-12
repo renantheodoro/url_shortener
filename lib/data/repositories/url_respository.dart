c9a0a41d700e8f05b57d1f9acfc85a1d569a5dd1
Renan Theodoro <renan.theodoro@simlabsdev.onmicrosoft.com>
2022-10-12 14:57:35 -0300

    feat: add url repository

---
+++ lib/data/repositories/url_respository.dart
@@ -0,0 +1,48 @@
+import 'package:url_shortener/core/error/exception.dart';
+import 'package:url_shortener/core/platform/network_info.dart';
+import 'package:url_shortener/data/datasource/local/url_local_data_source.dart';
+import 'package:url_shortener/data/datasource/remote/url_remote_data_source.dart';
+import 'package:url_shortener/data/models/url_model.dart';
+import 'package:url_shortener/domain/entities/url_entity.dart';
+import 'package:url_shortener/core/services/service_response.dart';
+import 'package:url_shortener/core/error/failures.dart';
+import 'package:url_shortener/domain/protocols/get_full_urls_protocol.dart';
+import 'package:url_shortener/domain/protocols/shorten_url_protocol.dart';
+
+class UrlRepository
+    implements ShortenUrlProtocol, GetFullUrlsProtocol {
+  UrlRepository(
+      {required this.localDataSource,
+      required this.remoteDataSource,
+      required this.networkInfo});
+
+  UrlLocalDataSourceInterface localDataSource;
+  UrlRemoteDataSourceInterface remoteDataSource;
+  NetworkInfo networkInfo;
+
+  @override
+  Future<ServiceResponse<Failure, UrlEntity>> shorten(String url) async {
+    bool? deviceIsConnected = await networkInfo.isConnected;
+
+    if (deviceIsConnected == true) {
+      try {
+        final UrlModel remoteData = await remoteDataSource.shorten(url);
+
+        await localDataSource.cacheUrl(remoteData);
+
+        return ServiceResponse.build(response: remoteData);
+      } on ServerException {
+        return ServiceResponse.build(failure: ServerFailure());
+      }
+    } else {
+      return ServiceResponse.build(failure: ConnectionFailure());
+    }
+  }
+
+  @override
+  Future<ServiceResponse<Failure, List<UrlEntity>>>
+      getCachedList() async {
+    final List<UrlModel> data = await localDataSource.getCachedList();
+    return ServiceResponse.build(response: data);
+  }
+}
